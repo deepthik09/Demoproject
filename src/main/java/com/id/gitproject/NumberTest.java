d5f129614f6d135316e70453f0987b7f647d4a32
konanki-24 <konankideepthi@gmail.com>
2024-05-17 14:48:51 -0500

    testing

--- src/main/java/com/id/gitproject/NumberTest.java
+++ src/main/java/com/id/gitproject/NumberTest.java
@@ -5,8 +5,13 @@ public class NumberTest {
 	public static void main(String[] args) {
 		for(int i=1;i<10;i++)
 		{
-			System.out.println(i);
+			for(int j=1;j<=i;j++)
+			{
+				System.out.println(j);
+			}
+			
 		}
+		
 
 	}
 
